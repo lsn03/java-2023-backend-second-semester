76d20a109e9e31c512bd6b62ce65b3f5fa7142f3
lsn03 <lvov.sn@mail.ru>
2024-02-13 12:46:43 +1000

    add some tests

---
+++ bot/src/test/java/edu/java/bot/hw1/UnknownCommandTest.java
@@ -0,0 +1,57 @@
+package edu.java.bot.hw1;
+
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.command.UnknownCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UnknownCommandTest {
+    @InjectMocks
+    private UnknownCommand unknownCommand;
+    private static final String TEXT = "text";
+    long id = 12345L;
+    Update update;
+    Message message;
+    Chat chat;
+
+    @BeforeEach
+    public void setUp() {
+        update = mock(Update.class);
+        message = mock(Message.class);
+        chat = mock(Chat.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "aboba",
+        "/pomogite",
+        "/avadocedavra",
+        "there is no help"
+    })
+    public void testUserUnknownCommand(String command) {
+        String expectedString = UnknownCommand.UNKNOWN_COMMAND_HELP;
+
+        when(update.message()).thenReturn(message);
+        when(message.chat()).thenReturn(chat);
+        when(chat.id()).thenReturn(id);
+        when(message.text()).thenReturn(command);
+
+        SendMessage response = unknownCommand.handle(update);
+
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+}
--- bot/src/test/java/edu/java/bot/hw1/UserNotRegisteredTest.java
+++ bot/src/test/java/edu/java/bot/hw1/UserNotRegisteredTest.java
@@ -3,89 +3,68 @@ package edu.java.bot.hw1;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import edu.java.bot.command.Command;
-import edu.java.bot.parser.GitHubHandler;
-import edu.java.bot.parser.ResourceHandler;
-import edu.java.bot.parser.StackOverFlowHandler;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.command.*;
 import edu.java.bot.processor.UserMessageProcessor;
-import edu.java.bot.service.CommandService;
-import java.util.ArrayList;
-import java.util.List;
 import edu.java.bot.service.LinkParserService;
 import edu.java.bot.storage.Storage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserMessageProcessorTest {
-    public static final String TEXT = "text";
+public class UserNotRegisteredTest {
+    private static final String TEXT = "text";
     long id = 12345L;
     Update update;
     Message message;
     Chat chat;
     String expectedString;
-
-
-
-    @Mock
-    private Command startCommand;
-    @Mock
-    private Command cancelCommand;
-    @Mock
-    private Command helpCommand;
-    @Mock
-    private Command listCommand;
-    @Mock
-    private Command trackCommand;
-    @Mock
-    private Command unTrackCommand;
     @Mock
     private Storage storage;
-    @Mock
-    private ResourceHandler github = new GitHubHandler();
-    @Mock
-    private ResourceHandler stackOverFlow = new StackOverFlowHandler();
+
+    @InjectMocks
+    private StartCommand startCommand;
+    @InjectMocks
+    private CancelCommand cancelCommand;
+    @InjectMocks
+    private HelpCommand helpCommand;
+    @InjectMocks
+    private ListCommand listCommand;
+    @InjectMocks
+    private TrackCommand trackCommand;
+    @InjectMocks
+    private UnTrackCommand unTrackCommand;
+    @InjectMocks
+    private UnknownCommand unknownCommand;
+
     @Mock
     private LinkParserService parserService;
-    private CommandService commandService;
+    @Mock
     private UserMessageProcessor userMessageProcessor;
+    @Mock
+    List<Command> commandList;
 
     @BeforeEach
     public void setUp() {
-
-        List<Command> commandList = new ArrayList<>();
-        lenient().when(startCommand.command()).thenReturn("/start");
-        lenient().when(cancelCommand.command()).thenReturn("/cancel");
-        lenient().when(helpCommand.command()).thenReturn("/help");
-        lenient().when(listCommand.command()).thenReturn("/list");
-        lenient().when(trackCommand.command()).thenReturn("/track");
-        lenient().when(unTrackCommand.command()).thenReturn("/untrack");
-
+        storage = mock(Storage.class);
+        parserService = mock(LinkParserService.class);
+        commandList = new ArrayList<>();
 
         commandList.add(cancelCommand);
         commandList.add(helpCommand);
         commandList.add(listCommand);
         commandList.add(trackCommand);
         commandList.add(unTrackCommand);
-
-
-
-
-
-        List<ResourceHandler> resourceHandlerList = new ArrayList<>();
-        resourceHandlerList.add(github);
-        resourceHandlerList.add(stackOverFlow);
-
-        parserService = new LinkParserService(resourceHandlerList);
-        commandService = new CommandService(storage,parserService);
-        userMessageProcessor = new UserMessageProcessor(commandList, storage);
+        commandList.add(unknownCommand);
 
         update = mock(Update.class);
         message = mock(Message.class);
@@ -99,27 +78,71 @@ public class UserMessageProcessorTest {
         when(message.text()).thenReturn(command);
     }
 
+    @Test
+    public void testUserNotRegisteredCancel() {
+        expectedString = CancelCommand.USER_NOT_REGISTERED;
+        Command currentCommand = cancelCommand;
+        defaultInit(id, currentCommand.command());
+        lenient().when(storage.isUserAuth(id)).thenReturn(false);
+        SendMessage response = currentCommand.handle(update);
 
-//    @ParameterizedTest()
-//    @CsvSource(value = {
-//        "/cancel",
-//        "/track",
-//        "/untrack",
-//        "/list",
-//        "/aboba",
-//        "/help",
-//        "hello"
-//    })
-//    public void testProcessUserNotRegistered(String command) {
-//        expectedString = UserMessageProcessor.USER_NOT_REGISTERED;
-//        defaultInit(id, command);
-//        SendMessage response = userMessageProcessor.process(update);
-//
-//        assertNotNull(response);
-//        assertEquals(expectedString, response.getParameters().get(TEXT));
-//
-//    }
-//
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+
+    @Test
+    public void testUserNotRegisteredHelp() {
+        expectedString = HelpCommand.USER_NOT_REGISTERED;
+        Command currentCommand = helpCommand;
+        defaultInit(id, currentCommand.command());
+        lenient().when(storage.isUserAuth(id)).thenReturn(false);
+        SendMessage response = currentCommand.handle(update);
+
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+
+    @Test
+    public void testUserNotRegisteredList() {
+        expectedString = ListCommand.USER_NOT_REGISTERED;
+        Command currentCommand = listCommand;
+        defaultInit(id, currentCommand.command());
+        lenient().when(storage.isUserAuth(id)).thenReturn(false);
+        SendMessage response = currentCommand.handle(update);
+
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+
+    @Test
+    public void testUserNotRegisteredTrack() {
+        expectedString = ListCommand.USER_NOT_REGISTERED;
+        Command currentCommand = trackCommand;
+        defaultInit(id, currentCommand.command());
+        lenient().when(storage.isUserAuth(id)).thenReturn(false);
+        SendMessage response = currentCommand.handle(update);
+
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+
+    @Test
+    public void testUserNotRegisteredUnTrack() {
+        expectedString = ListCommand.USER_NOT_REGISTERED;
+        Command currentCommand = unTrackCommand;
+        defaultInit(id, currentCommand.command());
+        lenient().when(storage.isUserAuth(id)).thenReturn(false);
+        SendMessage response = currentCommand.handle(update);
+
+        assertNotNull(response);
+        assertEquals(expectedString, response.getParameters().get(TEXT));
+
+    }
+
 //    @Test
 //    public void testStartCommand() {
 //        expectedString = UserMessageProcessor.USER_REGISTERED_SUCCESS;
