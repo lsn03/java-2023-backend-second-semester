9b99a407caa790d885a2e2e27862489b5986430e
lsn03 <lvov.sn@mail.ru>
2024-02-20 09:58:12 +1000

    refactor

--- scrapper/src/main/java/edu/java/controller/SOFController.java
+++ scrapper/src/main/java/edu/java/controller/SOFController.java
@@ -4,18 +4,18 @@ import edu.java.model.stack_over_flow.StackOverFlowModel;
 import edu.java.model.stack_over_flow.dto.QuestionAnswerDTO;
 import edu.java.model.stack_over_flow.dto.QuestionHeaderDTO;
 import edu.java.service.client.StackOverFlowClient;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/scrapper/sof")
 @Slf4j
-public class BotGitHubController {
+public class SOFController {
 
     private StackOverFlowClient service;
 
@@ -24,11 +24,13 @@ public class BotGitHubController {
         return service.fetchAnswers(6402162).block();
 
     }
+
     @GetMapping("/2")
     public QuestionHeaderDTO trackGitHubUrl2() {
         return service.fetchHeader(6402162).block();
 
     }
+
     @GetMapping("/3")
     public StackOverFlowModel trackGitHubUrl3() {
         return service.fetchQuestionData(6402162).block();
--- scrapper/src/main/java/edu/java/service/StackOverFlowService.java
+++ scrapper/src/main/java/edu/java/service/StackOverFlowService.java
@@ -1,10 +1,10 @@
 package edu.java.service;
 
 import edu.java.model.stack_over_flow.StackOverFlowModel;
-import edu.java.model.stack_over_flow.wrapper.StackOverFlowHeaderResponseWrapper;
-import edu.java.model.stack_over_flow.wrapper.StackOverFlowAnswerResponseWrapper;
 import edu.java.model.stack_over_flow.dto.QuestionAnswerDTO;
 import edu.java.model.stack_over_flow.dto.QuestionHeaderDTO;
+import edu.java.model.stack_over_flow.wrapper.StackOverFlowAnswerResponseWrapper;
+import edu.java.model.stack_over_flow.wrapper.StackOverFlowHeaderResponseWrapper;
 import edu.java.service.client.BaseUrl;
 import edu.java.service.client.StackOverFlowClient;
 import java.util.List;
@@ -48,16 +48,20 @@ public class StackOverFlowService implements StackOverFlowClient {
 
     @Override
     public Mono<List<QuestionAnswerDTO>> fetchAnswers(int questionId) {
-        return webClient.get().uri("/questions/{questionId}/answers?site=stackoverflow", questionId)
-            .retrieve().bodyToMono(StackOverFlowAnswerResponseWrapper.class)
+        return webClient.get()
+            .uri("/questions/{questionId}/answers?site=stackoverflow", questionId)
+            .retrieve()
+            .bodyToMono(StackOverFlowAnswerResponseWrapper.class)
             .map(StackOverFlowAnswerResponseWrapper::getQuestionAnswerDTOList);
 
     }
 
     @Override
     public Mono<QuestionHeaderDTO> fetchHeader(int questionId) {
-        return webClient.get().uri("/questions/{questionId}?order=desc&sort=activity&site=stackoverflow", questionId)
-            .retrieve().bodyToMono(StackOverFlowHeaderResponseWrapper.class)
+        return webClient.get()
+            .uri("/questions/{questionId}?order=desc&sort=activity&site=stackoverflow", questionId)
+            .retrieve()
+            .bodyToMono(StackOverFlowHeaderResponseWrapper.class)
             .map(stackOverFlowHeaderResponseWrapper -> stackOverFlowHeaderResponseWrapper.getList().get(0));
     }
 }
