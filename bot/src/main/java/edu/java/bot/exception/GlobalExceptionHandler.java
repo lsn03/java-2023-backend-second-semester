d12ff830be39654137f2a691b25e58b8a8823a7c
lsn03 <lvov.sn@mail.ru>
2024-02-22 16:49:07 +1000

    Done Bot handler

---
+++ bot/src/main/java/edu/java/bot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package edu.java.bot.exception;
+
+import edu.java.bot.model.dto.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    public static final String ERROR_INCORRECT_PARAMETERS = "Некорректные параметры запроса";
+    @ExceptionHandler(IncorrectParametersException.class)
+    public ResponseEntity<ApiErrorResponse> handleIncorrectParametersException(IncorrectParametersException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ApiErrorResponse errorResponse = new ApiErrorResponse();
+
+        errorResponse.setCode(String.valueOf(status.value()));
+        errorResponse.setExceptionMessage(ex.getMessage());
+        errorResponse.setExceptionName(IncorrectParametersException.class.getName());
+        errorResponse.setDescription(ERROR_INCORRECT_PARAMETERS);
+
+        errorResponse.setStacktrace(Arrays.stream(ex.getStackTrace())
+            .map(StackTraceElement::toString)
+            .collect(Collectors.toList()));
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+}
---
+++ bot/src/main/java/edu/java/bot/exception/IncorrectParametersException.java
@@ -0,0 +1,19 @@
+package edu.java.bot.exception;
+
+public class IncorrectParametersException extends RuntimeException {
+    public IncorrectParametersException() {
+        super();
+    }
+
+    public IncorrectParametersException(String message) {
+        super(message);
+    }
+
+    public IncorrectParametersException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IncorrectParametersException(Throwable cause) {
+        super(cause);
+    }
+}
