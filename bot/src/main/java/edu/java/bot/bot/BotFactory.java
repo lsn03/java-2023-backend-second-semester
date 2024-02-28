7c8b7e50d017e0a377faafb941c57507936d1642
lsn03 <lvov.sn@mail.ru>
2024-02-28 13:11:46 +1000

    fix checkstyle

--- bot/src/main/java/edu/java/bot/bot/BotFactory.java
+++ bot/src/main/java/edu/java/bot/bot/BotFactory.java
@@ -2,7 +2,6 @@ package edu.java.bot.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
