9b9431cf91af4e17d4a871e2ce98b22827fe8e7f
lsn03 <lvov.sn@mail.ru>
2024-02-29 21:13:21 +1000

    init hw4

---
+++ migrations/changelog-0.0.0.sql
@@ -0,0 +1,32 @@
+--liquibase formatted sql
+
+--changeset lsn03:1
+--comment first migration
+
+
+create table chat
+(
+    chat_id bigint not null
+        constraint chat_pk
+            primary key,
+    active  boolean default false
+);
+
+create table link
+(
+    link_id integer not null
+        constraint link_pk
+            primary key,
+    uri     text    not null
+);
+
+create table link_chat
+(
+    link_id integer
+        constraint link_to_user_link_link_id_fk
+            references link,
+    chat_id bigint
+        constraint link_to_user_user_user_id_fk
+            references chat
+);
+
