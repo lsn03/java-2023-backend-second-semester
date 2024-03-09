1cd5c0d96b7c2a6c4686d7935bbb4609ab40a80d
lsn03 <lvov.sn@mail.ru>
2024-03-09 14:25:32 +1000

    fix error test

--- scrapper/src/test/java/edu/java/scrapper/hw5/JdbcChatTest.java
+++ scrapper/src/test/java/edu/java/scrapper/hw5/JdbcChatTest.java
@@ -2,7 +2,8 @@ package edu.java.scrapper.hw5;
 
 import edu.java.domain.repository.jdbc.JdbcChatRepository;
 import edu.java.scrapper.IntegrationTest;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
